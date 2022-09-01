080fc6578ecd52be5b2f71173f7899f53dfa436d
asayed <ahanafy90@yahoo.com>
2022-09-01 13:57:56 +0200

    second-assignment-solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstTestComponent } from './first-test/first-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstTestComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/first-test/first-test.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-test',
+  templateUrl: './first-test.component.html',
+  styleUrls: ['./first-test.component.css']
+})
+export class FirstTestComponent implements OnInit {
+  displayDetails: boolean = true;
+  logEvent: number = 0;
+  logsArray: number[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  toggleDetails() {
+    this.displayDetails = !this.displayDetails;
+  }
+
+  logClick() {
+    this.logEvent++;
+    this.logsArray.push(this.logEvent);
+  }
+}
